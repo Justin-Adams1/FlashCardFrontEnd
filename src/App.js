5a755258da72909d20b635e06e1618795009a286
Justin Adams <khisanth05@gmail.com>
2021-05-04 20:37:53 -0400

    Major - Hooked up the front/back, and verified the front end has access to all collections. From here, work can begin on the data manipulation on the front end

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import axios from 'axios';
 import './App.css';
 import { Container } from 'react-bootstrap';
 
-function App() {
-  return (
-    <Container className="background" fluid>
-      Testing
-    </Container>
-  );
-}
+class App extends React.Component{
+
+  async apiCall(){
+  try{
+    const result = await axios.get("http://localhost:5000/api/collections/");
+    const data = result.data;
+
+    console.log(data);
+    
+    this.setState({data});
+
+  }
+  catch(error){
+    console.log(error);
+  }
+  }
+
+
+  componentDidMount(){
+    this.apiCall()
+  }
+
+  render(){  
+    return (
+      <Container className="background" fluid>
+        Testing
+      </Container>
+      );
+  }};
+
 
 export default App;
