531550efbf707a7dc8a4ff2ef7b21c8ad7753c38
Justin Adams <khisanth05@gmail.com>
2021-05-10 22:59:01 -0400

    Minor - Modal frames are looking correct. Need to get form data submit working

--- src/components/modifyCardModal.jsx
+++ src/components/modifyCardModal.jsx
@@ -28,19 +28,17 @@ class BootstrapModal extends React.Component{
                     </Modal.Header>
                     <Modal.Body>
                         <Form>
-                            <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
+                            <Form.Group controlId="cardTitle">
+                            <Form.Label>Card Title</Form.Label>
+                            <Form.Control type="text" placeholder="New Card Title Here" />
                             </Form.Group>
-                            <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Group controlId="DefinitionOne">
+                            <Form.Label>Definition One</Form.Label>
+                            <Form.Control type="text" placeholder="Definition One" />
                             </Form.Group>
-                            <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
+                            <Form.Group controlId="DefinitionOne">
+                            <Form.Label>Definition One</Form.Label>
+                            <Form.Control type="text" placeholder="Definition One" />
                             </Form.Group>
                             <Button variant="primary" type="submit">
                             Submit
--- src/components/modifyCollectionModal.jsx
+++ src/components/modifyCollectionModal.jsx
@@ -24,23 +24,13 @@ class BootstrapModal extends React.Component{
 
                 <Modal show={this.state.showHide}>
                     <Modal.Header closeButton onClick={() => this.handleModalShowHide()}>
-                    <Modal.Title>Modify Card</Modal.Title>
+                    <Modal.Title>Modify Collection</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <Form>
-                            <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                            </Form.Group>
-                            <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                            <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
+                            <Form.Group controlId="CollectionTitle">
+                            <Form.Label>Collection Title</Form.Label>
+                            <Form.Control type="text" placeholder="New Collection Name Here:" />
                             </Form.Group>
                             <Button variant="primary" type="submit">
                             Submit
